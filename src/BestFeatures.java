51d3268b0028473ad40ce8f454d8fb0a5b1d9ebd
Justin Wheeler <wheelers_webistes@yahoo.com>
2022-09-22 08:46:19 -0400

    Updated SwitchExpression example, Added more methods, Updated feature name to use System err so that it prints red

--- src/BestFeatures.java
+++ src/BestFeatures.java
@@ -2,15 +2,71 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 public class BestFeatures {
+    void collectionListOf(){
+        final List<Integer> old = Arrays.asList(1, 2, 3);
+        final List<Integer> cur = List.of(1, 2, 3);
 
+        System.err.println("\ncollectionListOf");
+        System.out.printf("old: %s%n", old);
+        System.out.printf("cur: %s%n", cur);
+    }
+
+    void collectionMapOf(){
+        final Map<Integer, Character> old = new HashMap<>() {{
+           put(1, 'a');
+           put(2, 'b');
+           put(3, 'c');
+        }};
+        // only works up to 10 elements
+        final Map<Integer, Character> cur = Map.of(1, 'a', 2, 'b', 3, 'c');
+
+        System.err.println("\ncollectionMapOf");
+        System.out.printf("old: %s%n", old);
+        System.out.printf("cur: %s%n", cur);
+    }
+
+    void collectionMapOfEntries(){
+        final Map<Integer, Character> old = new HashMap<>() {{
+            put(1, 'a');
+            put(2, 'b');
+            put(3, 'c');
+        }};
+        final Map<Integer, Character> cur = Map.ofEntries(
+            Map.entry(1, 'a'),
+            Map.entry(2, 'b'),
+            Map.entry(3, 'c')
+        );
+
+        System.err.println("\ncollectionMapOfEntries");
+        System.out.printf("old: %s%n", old);
+        System.out.printf("cur: %s%n", cur);
+    }
+
+    void collectionSetOf(){
+        final Set<Integer> old = new HashSet<>(Arrays.asList(1, 2, 3));
+        final Set<Integer> cur = Set.of(1, 2, 3);
+
+        System.err.println("\ncollectionSetOf");
+        System.out.printf("old: %s%n", old);
+        System.out.printf("cur: %s%n", cur);
+    }
 
     void collectorsToList(){
         final String[] val = new String[]{"val1", "val2"};
         final List<String> old = Arrays.stream(val).collect(Collectors.toList());
         final List<String> cur = Arrays.stream(val).toList();
 
-        System.out.println("\ncollectorsToList");
+        System.err.println("\ncollectorsToList");
         System.out.printf("val: %s%n", Arrays.toString(val));
+        System.out.printf("old: %s%n", old);
+        System.out.printf("cur: %s%n", cur);
+    }
+
+    void record(){
+        final ImmutableClass old = new ImmutableClass("justin");
+        final ImmutableRecord cur = new ImmutableRecord("justin");
+
+        System.err.println("\nrecord");
         System.out.printf("old: %s%n", old);
         System.out.printf("cur: %s%n", cur);
     }
@@ -37,49 +93,95 @@ public class BestFeatures {
             }
         }
 
-        System.out.println("\npatternMatchingInstanceOf");
+        System.err.println("\npatternMatchingInstanceOf");
         System.out.printf("old: %s%n", oldPetMap);
         System.out.printf("cur: %s%n", curPetMap);
     }
 
     void switchExpression(){
-        final Color[] colors = Color.values();
-        final Map<Color, String> oldColorGroupMap = new LinkedHashMap<>();
-        final Map<Color, String> newColorGroupMap = new LinkedHashMap<>();
+        final Map<Month, Integer> oldMonthDaysMap = new LinkedHashMap<>();
+        final Map<Month, Integer> curMonthDaysMap = new LinkedHashMap<>();
 
-        for(final Color c : colors){
-            String old;
-            switch(c){
-                case RED:
-                case ORANGE:
-                    old = "mostly red";
+        for(final Month m : Month.values()){
+            int oldDays;
+
+            switch(m){
+                case JANUARY, MARCH, MAY, JULY, AUGUST, OCTOBER, DECEMBER :
+                    oldDays = 31;
                     break;
-                case YELLOW:
-                case GREEN:
-                    old = "mostly green";
+                case FEBRUARY :
+                    oldDays = 28;
                     break;
-                case BLUE:
-                case INDIGO:
-                case VIOLET:
-                    old = "mostly blue";
+                case APRIL, JUNE, SEPTEMBER, NOVEMBER :
+                    oldDays = 30;
                     break;
                 default:
-                    throw new UnsupportedOperationException();
+                    throw new IllegalStateException();
             }
-            oldColorGroupMap.put(c, old);
+            oldMonthDaysMap.put(m, oldDays);
 
-            final String cur = switch(c){
-                case RED, ORANGE -> "mostly red";
-                case YELLOW, GREEN -> "mostly green";
-                case BLUE, INDIGO, VIOLET -> "mostly blue";
-                default -> throw new UnsupportedOperationException();
+            final int curDays = switch(m){
+                case JANUARY, MARCH, MAY, JULY, AUGUST, OCTOBER, DECEMBER -> 31;
+                case FEBRUARY -> 28;
+                case APRIL, JUNE, SEPTEMBER, NOVEMBER -> 30;
+                default -> throw new IllegalStateException();
             };
-            newColorGroupMap.put(c, cur);
+            curMonthDaysMap.put(m, curDays);
         }
 
-        System.out.println("\nswitchExpression");
-        System.out.printf("old: %s%n", oldColorGroupMap);
-        System.out.printf("cur: %s%n", newColorGroupMap);
+        System.err.println("\nswitchExpression");
+        System.out.printf("old: %s%n", oldMonthDaysMap);
+        System.out.printf("cur: %s%n", curMonthDaysMap);
+    }
+
+    void switchExpressionYield(){
+        final Map<Month, Integer> oldMonthDaysMap = new LinkedHashMap<>();
+        final Map<Month, Integer> curMonthDaysMap = new LinkedHashMap<>();
+
+        System.err.println("\nswitchExpressionYield");
+
+        for(final Month m : Month.values()) {
+            int oldDays;
+
+            switch (m) {
+                case JANUARY, MARCH, MAY, JULY, AUGUST, OCTOBER, DECEMBER:
+                    System.out.printf("\t%s: 31%n", m);
+                    oldDays = 31;
+                    break;
+                case FEBRUARY:
+                    System.out.printf("\t%s: 28%n", m);
+                    oldDays = 28;
+                    break;
+                case APRIL, JUNE, SEPTEMBER, NOVEMBER:
+                    System.out.printf("\t%s: 30%n", m);
+                    oldDays = 30;
+                    break;
+                default:
+                    throw new IllegalStateException();
+            }
+            oldMonthDaysMap.put(m, oldDays);
+        }
+        System.out.printf("\nold: %s%n%n", oldMonthDaysMap);
+
+        for(final Month m : Month.values()){
+            final int curDays = switch(m){
+                case JANUARY, MARCH, MAY, JULY, AUGUST, OCTOBER, DECEMBER -> {
+                    System.out.printf("\t%s: 31%n", m);
+                    yield 31;
+                }
+                case FEBRUARY -> {
+                    System.out.printf("\t%s: 28%n", m);
+                    yield 28;
+                }
+                case APRIL, JUNE, SEPTEMBER, NOVEMBER -> {
+                    System.out.printf("\t%s: 30%n", m);
+                    yield 30;
+                }
+                default -> throw new IllegalStateException();
+            };
+            curMonthDaysMap.put(m, curDays);
+        }
+        System.out.printf("\ncur: %s%n%n", curMonthDaysMap);
     }
 
     void textBlocks(){
@@ -92,13 +194,23 @@ public class BestFeatures {
                 }
                 """;
 
-        System.out.println("\ntextBlocks");
+        System.err.println("\ntextBlocks");
         System.out.printf("old: %s%n", old);
         System.out.printf("cur: %s%n", cur);
     }
 
-    enum Color {
-        RED, ORANGE, YELLOW, GREEN, BLUE, INDIGO, VIOLET
+    void var(){
+        final SuperHelpfulReallyLongAndDescriptiveClassName old = new SuperHelpfulReallyLongAndDescriptiveClassName();
+        final var cur = new SuperHelpfulReallyLongAndDescriptiveClassName();
+
+        System.err.println("\nvar");
+        System.out.printf("old: %s%n", old);
+        System.out.printf("cur: %s%n", cur);
+
+    }
+
+    enum Month {
+        JANUARY, FEBRUARY, MARCH, APRIL, MAY, JUNE, JULY, AUGUST, SEPTEMBER, OCTOBER, NOVEMBER, DECEMBER
     }
 
     interface Pet {
@@ -126,4 +238,43 @@ public class BestFeatures {
             return getClass().getName();
         }
     }
+
+    class SuperHelpfulReallyLongAndDescriptiveClassName {
+        @Override
+        public String toString() {
+            return getClass().getName();
+        }
+    }
+
+    class ImmutableClass {
+        private final String name;
+
+        ImmutableClass(String name) {
+            this.name = name;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o)
+                return true;
+            if (o == null || getClass() != o.getClass())
+                return false;
+            ImmutableClass that = (ImmutableClass) o;
+            return Objects.equals(name, that.name);
+        }
+        @Override
+        public int hashCode() {
+            return Objects.hash(name);
+        }
+        @Override
+        public String toString() {
+            return String.format("ImmutableClass[name=%s]", name);
+        }
+    }
+
+    record ImmutableRecord(String name){}
 }
--- src/BestFeaturesTest.java
+++ src/BestFeaturesTest.java
@@ -11,8 +11,33 @@ class BestFeaturesTest {
     }
 
     @Test
+    void collectionListOf(){
+        bestFeatures.collectionListOf();
+    }
+
+    @Test
+    void collectionMapOf(){
+        bestFeatures.collectionMapOf();
+    }
+
+    @Test
+    void collectionMapOfEntries(){
+        bestFeatures.collectionMapOfEntries();
+    }
+
+    @Test
+    void collectionSetOf(){
+        bestFeatures.collectionSetOf();
+    }
+
+    @Test
     void collectorsToList(){
         bestFeatures.collectorsToList();
+    }
+
+    @Test
+    void record(){
+        bestFeatures.record();
     }
 
     @Test
@@ -26,7 +51,17 @@ class BestFeaturesTest {
     }
 
     @Test
+    void switchExpressionYield(){
+        bestFeatures.switchExpressionYield();
+    }
+
+    @Test
     void textBlocks(){
         bestFeatures.textBlocks();
+    }
+
+    @Test
+    void var(){
+        bestFeatures.var();
     }
 }
